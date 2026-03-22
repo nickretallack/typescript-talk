1d2f98f5f3a31f91b05830d332f1b933e5af45a6
Nick Retallack <nicholas.retallack@contractors.scale.com>
2026-03-22 10:58:28 -0700

    reworked the node versions section

---
+++ 1-commonjs-vs-esm/7-node-CJS-ESM-interop/20.17/run.sh
@@ -0,0 +1,3 @@
+node ../src/import-cjs.mjs # works
+node ../src/import-mjs.cjs # fails
+node ../src/import-cjs.mjs --experimental-require-module # works
---
+++ 1-commonjs-vs-esm/7-node-CJS-ESM-interop/22.13/run.sh
@@ -0,0 +1,2 @@
+node ../src/import-cjs.mjs # works
+node ../src/import-mjs.cjs # works
